860893a88a8f013d22a81d7b75ce45c70beaa8cb
Guhansv <guhan1805@gmail.com>
2023-03-14 13:08:36 +0530

    changes

--- src/components/Apparel.jsx
+++ src/components/Apparel.jsx
@@ -5,26 +5,37 @@ function Apparel() {
     <div className='apparel-container'>
        
     <div class="max-w-sm rounded overflow-hidden shadow-lg img-container backgrd-clr">
-        <img class="w-full" src="https://wpcdn.bigbathroomshop.co.uk/info/blog/wp-content/uploads/2021/08/filling-a-bathtub-with-water-QTPKNG4.jpg?strip=all&lossy=1&quality=70&resize=900%2C599&ssl=1" alt="Sunset in the mountains"/>
+        <img class="w-full" src="https://wpcdn.bigbathroomshop.co.uk/info/blog/wp-content/uploads/2021/08/filling-a-bathtub-with-water-QTPKNG4.jpg?strip=all&lossy=1&quality=70&resize=900%2C599&ssl=1" alt="product-img"/>
          <div class="px-6 py-4 ">
             <div class="font-bold text-xl mb-2">Cv's Bathwater</div>
             <p class="text-gray-700 text-base">$6969</p>
+            <button type="button" class="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 add-tocart">
+                    <svg aria-hidden="true" class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg> Add to cart</button>
         </div>
+        
     </div>
 
     <div class="max-w-sm rounded overflow-hidden shadow-lg img-container backgrd-clr">
-        <img class="w-full" src="https://contents.mediadecathlon.com/p1961521/3d03967ddebb50c02023f38e5120807d/p1961521.jpg?format=auto&quality=70&f=650x0" alt="Sunset in the mountains"/>
+        <img class="w-full" src="https://contents.mediadecathlon.com/p1961521/3d03967ddebb50c02023f38e5120807d/p1961521.jpg?format=auto&quality=70&f=650x0" alt="product-img"/>
          <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">Cv's Socks</div>
             <p class="text-gray-700 text-base">$6969</p>
+            <button type="button" class="text-white  bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 add-tocart">
+                <svg aria-hidden="true" class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                     Add to cart
+            </button>
         </div>
     </div>
 
     <div class="max-w-sm rounded overflow-hidden shadow-lg img-container backgrd-clr">
-        <img class="w-full" src="https://www.berluti.com/dw/image/v2/BBMR_PRD/on/demandware.static/-/Sites-masterCatalog_Berluti/default/dwff6de3da/images/R24JRS102-002_all-over-scritto-jacquard-t-shirt_pearl-grey_berluti_01.jpg?sw=320" alt="Sunset in the mountains"/>
+        <img class="w-full" src="https://www.berluti.com/dw/image/v2/BBMR_PRD/on/demandware.static/-/Sites-masterCatalog_Berluti/default/dwff6de3da/images/R24JRS102-002_all-over-scritto-jacquard-t-shirt_pearl-grey_berluti_01.jpg?sw=320" alt="product-img"/>
          <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">Cv's Used Tee</div>
             <p class="text-gray-700 text-base">$6969</p>
+            <button type="button" class="text-white bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-black font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 add-tocart">
+                <svg aria-hidden="true" class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                     Add to cart
+            </button>
         </div>
     </div>
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Components() {
+function Contact() {
   return (
     <div>
       
     </div>
   )
 }
-export default  Components;
+export default Contact;
