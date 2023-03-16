aba5141d35a4b87934ea792b30d3275d20f3ba6b
Guhansv <guhan1805@gmail.com>
2023-03-16 18:51:52 +0530

    uploading

--- src/components/Apparels/Apparel.jsx
+++ src/components/Apparels/Apparel.jsx
@@ -5,7 +5,7 @@ import Shirts from './Shirts';
 function Apparel() {
   return (
     <div>
-    <div className='title-container'><h1 className='text-4xl flex justify-center text-white' id='contact'>See what the hype is about</h1></div>  
+    <div className='title-container'><h1 className='text-4xl flex justify-center text-white' id='apparels'>See what the hype is about</h1></div>  
     <div className='apparel-container'>
     <Shirts/>
     <Socks/>
